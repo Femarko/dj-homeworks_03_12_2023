8fa9faf6d83bd5e901d2af60566f4fe7326452ea
Femarko <fenstermark7@gmail.com>
2023-12-03 07:07:00 +0100

    Initial - restoration

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    description = models.CharField(null=True, blank=True)
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.FloatField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,51 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer, SensorDetailSerializer
+
+
+class ListCreateAPIView(ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    def post(self, request):
+        entry = Sensor(name=request.data.get('name'), description=request.data.get('description'))
+        entry.save()
+        serialized_entry = SensorSerializer(entry)
+        return Response(serialized_entry.data)
+
+class RetrieveUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    def patch(self, request, pk): # обновление датчика
+        entry = Sensor.objects.get(pk=pk)
+        entry.name = request.data.get('name')
+        entry.description = request.data.get('description')
+        entry.save()
+        serialized_entry = SensorSerializer(entry)
+        return Response(serialized_entry.data)
+
+    def get(self, request, pk): # получение информации по датчику
+        serialized_entry = SensorDetailSerializer(Sensor.objects.get(pk=pk))
+        print(Sensor.objects.get(pk=pk))
+        print(serialized_entry.data)
+        return Response(serialized_entry.data)
+
+
+class CreateAPIView(APIView):
+    def get(self, request):
+        return Response({'status': 'OK'})
+    def post(self, request):
+        sensor = Sensor.objects.get(pk=request.data.get('sensor')) # так работает
+        # а так:
+        # sensor = Sensor.objects.get(pk=request.data.get('sensor')).id #- возвращается ошибка
+        # "Cannot assign "1": "Measurement.sensor_id" must be a "Sensor" instance".
+        entry = Measurement(sensor_id=sensor, temperature=request.data.get('temperature'))
+        entry.save()
+        serialized_entry = MeasurementSerializer(entry)
+        return Response(serialized_entry.data)
